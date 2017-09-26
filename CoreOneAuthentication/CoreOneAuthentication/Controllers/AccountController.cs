feeb23f5aa0e0f8ca05b28cad1bab03e614bcde9
lucaspolaquini <cmte.lucas@hotmail.com>
2017-09-25 21:05:40 -0300

    Inicial

---
+++ CoreOneAuthentication/CoreOneAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreOneAuthentication.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace CoreOneAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User user)
+        {
+            if (user.UserName == "lucas" && user.Password == "123Mudar")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                var id = new ClaimsIdentity(claims, "password");
+                var principal = new ClaimsPrincipal(id);
+
+                await HttpContext.SignInAsync("app", principal);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("app");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ CoreOneAuthentication/CoreOneAuthentication/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreOneAuthentication.Models
+{
+    public class User
+    {
+        public String UserName { get; set; }
+
+        public String Password { get; set; }
+    }
+}
